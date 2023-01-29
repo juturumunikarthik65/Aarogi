726c0e102380ff9aaec777365699e869e05f68f3
girish1208dev <girishsampath1208@gmail.com>
2023-01-29 22:51:30 +0530

    created new elements

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 let profileIcon = document.querySelector('#profile-icon');
-let name = localStorage.getItem("name");
-let nameEl = document.querySelector('#name');
-nameEl.innerHTML = name;
 profileIcon.addEventListener('click', () => {
     // code for creating side navigation
     document.querySelector("#side-nav").style.width = "60%";
@@ -14,6 +11,78 @@ let sideNavClose = document.querySelector('#side-nav-close');
 sideNavClose.addEventListener('click', () => {
     sideNav.style.display = "none";
 });
+
+document.getElementById("details").addEventListener("click", function(){
+  var navdet=document.getElementById("nav-details");
+  navdet.scrollIntoView({ behavior: 'smooth' });
+  var navde=document.getElementById("nav-detail");
+  navde.scrollIntoView({ behavior: 'smooth' });
+  document.getElementById("side-nav").style.display="none";
+});
+
+document.getElementById("tips").addEventListener("click", function(){
+  var navdet=document.getElementById("nav-tips");
+  navdet.scrollIntoView({ behavior: 'smooth' });
+  var navde=document.getElementById("nav-tip");
+  navde.scrollIntoView({ behavior: 'smooth' });
+  document.getElementById("side-nav").style.display="none";
+});
+
+document.getElementById("benifits").addEventListener("click", function(){
+  var navdet=document.getElementById("nav-benifits");
+  navdet.scrollIntoView({ behavior: 'smooth' });
+  var navde=document.getElementById("nav-benifit");
+  navde.scrollIntoView({ behavior: 'smooth' });
+  document.getElementById("side-nav").style.display="none";
+});
+
+document.getElementById("FAQs").addEventListener("click", function(){
+  var navdet=document.getElementById("faq-section");
+  navdet.scrollIntoView({ behavior: 'smooth' });
+  
+  document.getElementById("side-nav").style.display="none";
+});
+
+
+window.addEventListener("load", function() {
+  const submitBtn = document.getElementById("submitBtn");
+  const nameInput = document.getElementById("nameInput");
+  const welcomeMessage = document.getElementById("welcomeMessage");
+  const enterNameText = document.getElementById("enterNameText");
+
+  submitBtn.addEventListener("click", function() {
+    const name = nameInput.value;
+    if (!name) {
+      alert("Please enter your name");
+      return;
+    }
+    localStorage.setItem("name", name);
+    welcomeMessage.innerHTML = `Welcome ${name}`;
+    nameInput.style.display = "none";
+    submitBtn.style.display = "none";
+    enterNameText.style.display = "none";
+  });
+
+  const name = localStorage.getItem("name");
+
+  if (name) {
+    welcomeMessage.innerHTML = `Welcome ${name}`;
+    nameInput.style.display = "none";
+    submitBtn.style.display = "none";
+    enterNameText.style.display = "none";
+  }
+});
+
+// Delay the execution of the code for 10 seconds (10,000 milliseconds)
+setTimeout(function() {
+  // Check if a name has been stored in local storage
+  var name = localStorage.getItem("name");
+  if (!name) {
+    // Display an alert if a name has not been stored
+    alert("Please input your name.");
+  }
+}, 10000);
+
 
 
 let leftRectangle = document.querySelector('.rectangle-bar.left');
@@ -43,9 +112,7 @@ function GetStart(){
     window.location.href = 'formpage.html';
 }
 
-
-
-  
-
-
-
+document.getElementById("goto50+").addEventListener("click", function(){
+  rightRectangle.scrollIntoView({ behavior: 'smooth' });
+  this.innerHTML="Go Top";
+});
