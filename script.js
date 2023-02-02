a2b7c5e00f1339f14ad452eb579f2a865efc2013
girish1208dev <girishsampath1208@gmail.com>
2023-02-02 20:11:50 +0530

    some changes

--- script.js
+++ script.js
@@ -13,24 +13,19 @@ sideNavClose.addEventListener('click', () => {
 });
 
 document.getElementById("details").addEventListener("click", function(){
-  var navdet=document.getElementById("nav-details");
-  navdet.scrollIntoView({ behavior: 'smooth' });
   var navde=document.getElementById("nav-detail");
   navde.scrollIntoView({ behavior: 'smooth' });
   document.getElementById("side-nav").style.display="none";
 });
 
 document.getElementById("tips").addEventListener("click", function(){
-  var navdet=document.getElementById("nav-tips");
-  navdet.scrollIntoView({ behavior: 'smooth' });
+ 
   var navde=document.getElementById("nav-tip");
   navde.scrollIntoView({ behavior: 'smooth' });
   document.getElementById("side-nav").style.display="none";
 });
 
 document.getElementById("benifits").addEventListener("click", function(){
-  var navdet=document.getElementById("nav-benifits");
-  navdet.scrollIntoView({ behavior: 'smooth' });
   var navde=document.getElementById("nav-benifit");
   navde.scrollIntoView({ behavior: 'smooth' });
   document.getElementById("side-nav").style.display="none";
@@ -43,101 +38,45 @@ document.getElementById("FAQs").addEventListener("click", function(){
   document.getElementById("side-nav").style.display="none";
 });
 
+window.addEventListener("load", function() {
+  const submitBtn = document.getElementById("submitBtn");
+  const nameInput = document.getElementById("nameInput");
+  const welcomeMessage = document.getElementById("welcomeMessage");
+  const enterNameText = document.getElementById("enterNameText");
 
-
-
-let leftRectangle = document.querySelector('.rectangle-bar.left');
-let rightRectangle = document.querySelector('.rectangle-bar.right');
-const leftInfo = document.querySelector("#image-container");
-const righttInfo = document.querySelector("#image-container1");
-
-rightRectangle.addEventListener('click', function() {
-    leftRectangle.style.backgroundColor = '#f5f5f5';
-    leftRectangle.style.color = '#333';
-    rightRectangle.style.backgroundColor = '#333';
-    rightRectangle.style.color = '#f5f5f5';
-    leftInfo.style.display = 'none';
-    righttInfo.style.display='block';
-});
-
-leftRectangle.addEventListener('click', function() {
-    leftRectangle.style.backgroundColor = '#333';
-    leftRectangle.style.color = '#f5f5f5';
-    rightRectangle.style.backgroundColor = '#f5f5f5';
-    rightRectangle.style.color = '#333';
-    leftInfo.style.display = 'block';
-    righttInfo.style.display='none';
-});
-
-
-
-document.getElementById("goto50+").addEventListener("click", function(){
-  rightRectangle.scrollIntoView({ behavior: 'smooth' });
-  this.innerHTML="Go Top";
-});
-
-
-const questions = document.querySelectorAll('.question');
-let currentAnswer;
-
-questions.forEach(question => {
-  question.addEventListener('click', () => {
-    if (currentAnswer) {
-      currentAnswer.classList.remove('show');
+  submitBtn.addEventListener("click", function() {
+    const name = nameInput.value;
+    if (!name) {
+      alert("Please enter your name");
+      return;
     }
-    currentAnswer = question.nextElementSibling;
-    currentAnswer.classList.toggle('show');
+    localStorage.setItem("name", name);
+    welcomeMessage.innerHTML = `Welcome ${name}`;
+    nameInput.style.display = "none";
+    submitBtn.style.display = "none";
+    enterNameText.style.display = "none";
   });
+
+  const name = localStorage.getItem("name");
+
+  if (name) {
+    welcomeMessage.innerHTML = `Welcome ${name}`;
+    nameInput.style.display = "none";
+    submitBtn.style.display = "none";
+    enterNameText.style.display = "none";
+  }
 });
 
+// Delay the execution of the code for 10 seconds (10,000 milliseconds)
+setTimeout(function() {
+  // Check if a name has been stored in local storage
+  var name = localStorage.getItem("name");
+  if (!name) {
+    // Display an alert if a name has not been stored
+    alert("Please input your name.");
+  }
+}, 10000);
 
-
-const registerButton = document.getElementById("register-button");
-
-
-const formOverlay = document.getElementById("form-overlay");
-const backButton = document.getElementById("back-button");
-
-registerButton.addEventListener("click", function() {
-  formOverlay.style.display = "block";
-  document.body.style.overflow = "hidden";
-});
-
-backButton.addEventListener("click", function() {
-  formOverlay.style.display = "none";
-  document.body.style.filter = "none";
-  document.body.style.overflow = "auto";
-});
-
-const registerButton1 = document.getElementById("register1");
-const formOverlay1 = document.getElementById("form-overlay1");
-const backButton1 = document.getElementById("back-button1");
-
-registerButton1.addEventListener("click", function() {
-  formOverlay1.style.display = "block";
-  document.body.style.overflow = "hidden";
-});
-
-backButton1.addEventListener("click", function() {
-  formOverlay1.style.display = "none";
-  document.body.style.filter = "none";
-  document.body.style.overflow = "auto";
-});
-
-const registerButton2 = document.getElementById("register2");
-const formOverlay2 = document.getElementById("form-overlay2");
-const backButton2 = document.getElementById("back-button2");
-
-registerButton2.addEventListener("click", function() {
-  formOverlay2.style.display = "block";
-  document.body.style.overflow = "hidden";
-});
-
-backButton2.addEventListener("click", function() {
-  formOverlay2.style.display = "none";
-  document.body.style.filter = "none";
-  document.body.style.overflow = "auto";
-});
 
 const registerButton3 = document.getElementById("register3");
 const formOverlay3 = document.getElementById("form-overlay3");
@@ -182,4 +121,23 @@ backButton5.addEventListener("click", function() {
   formOverlay5.style.display = "none";
   document.body.style.filter = "none";
   document.body.style.overflow = "auto";
+});
+
+document.getElementById("goto50+").addEventListener("click", function(){
+  var rightRectangle=document.getElementById("home");
+  rightRectangle.scrollIntoView({ behavior: 'smooth' });
+});
+
+
+const questions = document.querySelectorAll('.question');
+let currentAnswer;
+
+questions.forEach(question => {
+  question.addEventListener('click', () => {
+    if (currentAnswer) {
+      currentAnswer.classList.remove('show');
+    }
+    currentAnswer = question.nextElementSibling;
+    currentAnswer.classList.toggle('show');
+  });
 });
